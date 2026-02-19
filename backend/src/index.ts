8ec3581cb846ada9f8d7ae89c4126b4ebb4108f3
Kunal Rana <kunalranarj2005@gmail.com>
2026-02-20 02:31:27 +0530

    stuff

--- backend/src/index.ts
+++ backend/src/index.ts
@@ -1,11 +1,19 @@
+/** @format */
+
 import express from "express";
 import { config } from "./config/env";
 import analyzeRouter from "./routes/analyze.route";
+import cors from "cors";
 
 const app = express();
 
 // Middleware
 app.use(express.json());
+app.use(
+  cors({
+    origin: "*",
+  }),
+);
 app.use(express.urlencoded({ extended: true }));
 
 // Routes
